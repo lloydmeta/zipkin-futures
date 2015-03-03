7fbc349a10bf2127f5d30759e5ca79f563ce1ab6
Lloyd <lloydmeta@gmail.com>
2015-03-03 18:01:48 +0900

    Add comment

--- zipkin-futures-core/src/main/scala/com/beachape/zipkin/services/BraveZipkinService.scala
+++ zipkin-futures-core/src/main/scala/com/beachape/zipkin/services/BraveZipkinService.scala
@@ -18,9 +18,12 @@ import scala.concurrent.{ ExecutionContext, Future }
  * @param clientTraceFilters a List of span filters for client spans. List can be empty if you don't want trace filtering (sampling).
  *                           The trace filters will be executed in order. If one returns false there will not be tracing and
  *                           the next trace filters will not be executed anymore.
+ *
+ *                           In a typical server-side app, it might be a good idea to have a `{ s => s.isSetParent_id }` in here
+ *                           in case you want to make sure no orphaned client-spans get sent.
  * @param serverTraceFilters List of span filters for client spans. List can be empty if you don't want trace filtering (sampling).
- *                     The trace filters will be executed in order. If one returns false there will not be tracing and
- *                     the next trace filters will not be executed anymore.
+ *                           The trace filters will be executed in order. If one returns false there will not be tracing and
+ *                           the next trace filters will not be executed anymore.
  */
 class BraveZipkinService(hostIp: String,
                          hostPort: Int,
